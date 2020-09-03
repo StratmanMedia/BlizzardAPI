bc68de55ff758777f94485fd8e2007c90730af75
Jason Stratman <stratmanmedia@gmail.com>
2020-09-03 16:57:27 -0500

    Added back accidentally deleted Config class

---
+++ BlizzardAPI.Client.Tests/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace BlizzardAPI.Client.Tests
+{
+    public class Config
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
